7b33534d180e72859ef226aefa8754cddb0d3dff
alisyap28 <alisyap28@gmail.com>
2021-12-06 20:36:52 +0800

    create/add project

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+
+import io
+from flask import Flask, request
+from keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import load_model
+from flask_cors import cross_origin
+import pandas as pd
+import base64
+from PIL import Image
+
+
+def preprocess_base64image(base64img):
+    splitted_base64 = base64img.split(',')
+    filtered_base64 = splitted_base64[1]
+    base64_in_bytes = filtered_base64.encode('ascii')
+    decoded_img = base64.b64decode(base64_in_bytes)
+    img = Image.open(io.BytesIO(decoded_img))
+    return img
+
+
+def preprocess_image_dimension(raw_img):
+    img = raw_img.resize((150, 150))
+    img = img_to_array(img)
+    img = img.reshape(-1, 150, 150, 3)
+    return img
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    model = load_model("./model_green_scene.h5")
+
+    @app.route("/check-green-scene", methods=["POST"])
+    @cross_origin()
+    def checkGreenScene():
+        base64img = request.json['imageencoded']
+        rawimg = preprocess_base64image(base64img)
+        img = preprocess_image_dimension(rawimg)
+        result = model.predict(img)
+        return str(int(result[0][0]))
+
+    return app
