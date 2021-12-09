76fd6991ef68138c57645876ef7e1ac162353d6f
alisyap28 <alisyap28@gmail.com>
2021-12-09 22:05:21 +0800

    create/fix-png-format

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def preprocess_base64image(base64img):
     base64_in_bytes = filtered_base64.encode('ascii')
     decoded_img = base64.b64decode(base64_in_bytes)
     img = Image.open(io.BytesIO(decoded_img))
+    img = img.convert('RGB')
     return img
 
 
@@ -26,6 +27,7 @@ def preprocess_image_dimension(raw_img):
     img = img_to_array(img)
     img = img.reshape(-1, 150, 150, 3)
     return img
+
 
 @app.route("/check-green-scene", methods=["POST"])
 @cross_origin()
@@ -36,24 +38,7 @@ def checkGreenScene():
     result = model.predict(img)
     return str(int(result[0][0]))
 
+
 if __name__ == "__main__":
-    
     model = load_model("./model_green_scene.h5")
     app.run(host="0.0.0.0", port="5000", debug=True)
-
-# def create_app(test_config=None):
-#     app = Flask(__name__)
-#     model = load_model("./model_green_scene.h5")
-
-#     @app.route("/check-green-scene", methods=["POST"])
-#     @cross_origin()
-#     def checkGreenScene():
-#         base64img = request.json['imageencoded']
-#         rawimg = preprocess_base64image(base64img)
-#         img = preprocess_image_dimension(rawimg)
-#         result = model.predict(img)
-#         return str(int(result[0][0]))
-
-    # return 
-    
-
